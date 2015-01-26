7841aed0333b4941af30ebefa013f0997a492f4e
Andy Griffin <griffinandyb@gmail.com>
2015-01-25 23:23:28 -0800

    Initial Commit

---
+++ article.php
@@ -0,0 +1,23 @@
+<?php
+	$path = 'articles/'.$_GET['title'].'/';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="../styles/article_base.css" />
+	<?php require $path.'head.inc'; ?>
+</head>
+<body>
+
+	<header></header>
+	
+	<article class="article_body">
+		<?php require $path.'article.inc'; ?>
+	</article>
+	
+	<footer></footer>
+
+</body>
+</html>
